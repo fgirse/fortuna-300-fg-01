2e196b75e8358c34bdeb9c94f13930ff0cdd3327
fgirse <fgirse@bluewin.ch>
2022-06-10 16:52:15 +0200

    Commit Nr. 8

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import images from './images';
 //import meal from '../menu.png';
-import data from './data';
+import data from './dataDinks';
 
 export { images, data };
